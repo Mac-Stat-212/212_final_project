e00e12413addf1a9cc4c64d73c566f5a6e6a14ca
alanschulz12 <aschulzd@macalester.edu>
2024-11-04 15:16:04 -0600

    OH_brianna
    done

--- code/Cleaning.R
+++ code/Cleaning.R
@@ -1,23 +1,36 @@
 library(tidyverse)
-dat <- read.csv("212_final_project/data/All_Tickers_5_Year_Data.csv")
+dat <- read.csv("../data/All_Tickers_5_Year_Data.csv")
+
+
+dat_clean <- dat
+#dat_clean <- dat[ , colSums(is.na(dat))==0]
+dat_clean$Date <- as.Date(dat$Date)
 
 
 
-dat_clean <- dat[ , colSums(is.na(dat))==0]
-dat_clean$Date <- as.Date(dat_clean$Date)
-
-
-
-dat_pivot_longer <- pivot_longer(dat_clean, cols=2:35767, names_to="stock", values_to="values")
+dat_pivot_longer <- pivot_longer(dat_clean, cols= -Date, names_to="stock", values_to="values")
 
 
 
 dat_separated <- dat_pivot_longer %>%
   mutate(stock_name = str_extract(stock, "[^_]+"), bench_mark = str_extract(stock, "(?<=_).*") )
 
+dat_separated <- dat_separated %>%
+  filter(!is.na(values))
 
+
+
+
+rm(dat_pivot_longer,dat_clean,dat)
 
 dat_wider <- dat_separated %>%
   pivot_wider(names_from=bench_mark, values_from=values)
 
-write_csv(dat_wider,file = '../data/Long_Data.csv')
+
+
+write_csv(dat_wider,file = '../data/long_data.csv')
+
+dat_wider <- read_csv('../data/long_data.csv')
+dat_wider_sub <- dat_wider %>% slice_sample(n = 50000)
+
+write_csv(dat_wider_sub,file = '../data/long_data_sub.csv')
